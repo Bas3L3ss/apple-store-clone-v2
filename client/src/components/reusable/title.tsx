a217cb7ea195e3fad0a08949efbf6f5cad8f8f0a
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-26 16:37:05 +0700

    test: test github workflow #2

--- client/src/components/reusable/title.tsx
+++ client/src/components/reusable/title.tsx
@@ -8,7 +8,8 @@ const Title = ({
   children: ReactNode;
   className: string;
 }) => {
-  // return <h1 className={cn("title-margin", className)}>{children}</h1>;
+  d;
+  return <h1 className={cn("title-margin", className)}>{children}</h1>;
 };
 
 export default Title;
