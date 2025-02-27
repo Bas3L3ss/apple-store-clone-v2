8abc8c0e1ad2c159ede35a694704c540727475a9
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-27 14:58:16 +0700

    fix: type error of client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-//
+//@ts-expect-error
 import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      //@ts-expect-error
       "@": path.resolve(__dirname, "./"),
     },
   },
